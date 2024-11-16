3a99ae838dba78704edcfd5c6d817dc4f1543d17
NabilChabab <nabil.chababnabil@gmail.com>
2024-11-16 16:09:17 +0100

    Field CRUD

--- src/main/java/com/example/citronix/services/FieldService.java
+++ src/main/java/com/example/citronix/services/FieldService.java
@@ -1,4 +1,16 @@
 package com.example.citronix.services;
 
+import com.example.citronix.model.Field;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.UUID;
+
 public interface FieldService {
+
+    Field save(UUID farmUuid, Field field);
+    Field update(UUID fieldUuid, Field field);
+    Field findById(UUID fieldUuid);
+    Page<Field> findAllByFarm(UUID farmUuid, Pageable pageable);
+    void delete(UUID fieldUuid);
 }
