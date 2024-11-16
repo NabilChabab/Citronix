e3a4b65244774514074e6c4122b54afd01c1735d
NabilChabab <nabil.chababnabil@gmail.com>
2024-11-16 18:36:02 +0100

    Sale CRUD

---
+++ src/main/java/com/example/citronix/services/dto/SaleDTO.java
@@ -0,0 +1,20 @@
+package com.example.citronix.services.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SaleDTO {
+
+    private LocalDate saleDate;
+    private double unitPrice;
+    private String client;
+    private double revenue;
+}
