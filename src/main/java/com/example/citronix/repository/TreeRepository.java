fbb0375f31c8bf7e0d56565e345f184bca7b1d67
NabilChabab <nabil.chababnabil@gmail.com>
2024-11-16 16:36:27 +0100

    Tree CRUD

--- src/main/java/com/example/citronix/repository/TreeRepository.java
+++ src/main/java/com/example/citronix/repository/TreeRepository.java
@@ -3,7 +3,10 @@ package com.example.citronix.repository;
 import com.example.citronix.model.Tree;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface TreeRepository extends JpaRepository<Tree , UUID> {
+    long countByFieldUuid(UUID fieldUuid);
+    List<Tree> findAllByFieldUuid(UUID fieldUuid);
 }
