d157bda9649f03036d21dbd338475fa842c9c6cc
NabilChabab <nabil.chababnabil@gmail.com>
2024-11-16 18:35:29 +0100

    Harvest CRUD

--- src/main/java/com/example/citronix/repository/HarvestDetailRepository.java
+++ src/main/java/com/example/citronix/repository/HarvestDetailRepository.java
@@ -1,9 +1,12 @@
 package com.example.citronix.repository;
 
 import com.example.citronix.model.HarvestDetail;
+import com.example.citronix.model.Tree;
+import com.example.citronix.model.enums.Season;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface HarvestDetailRepository extends JpaRepository<HarvestDetail , UUID> {
+    boolean existsByTreeAndHarvest_Season(Tree tree, Season season);
 }
