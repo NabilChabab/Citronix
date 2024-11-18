b14b09546b9fef778db5876abe83feaf6d4d1103
NabilChabab <nabil.chababnabil@gmail.com>
2024-11-18 17:21:53 +0100

    HundleExceptions

---
+++ src/main/java/com/example/citronix/exceptions/HarvestPlantingException.java
@@ -0,0 +1,7 @@
+package com.example.citronix.exceptions;
+
+public class HarvestPlantingException extends RuntimeException {
+    public HarvestPlantingException(String message) {
+        super(message);
+    }
+}
