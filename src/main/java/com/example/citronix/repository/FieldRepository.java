19ca102fb4d48e5f50e52ada35135895cdf9d8fc
NabilChabab <nabil.chababnabil@gmail.com>
2024-11-18 17:20:25 +0100

    Quick Fix

--- src/main/java/com/example/citronix/repository/FieldRepository.java
+++ src/main/java/com/example/citronix/repository/FieldRepository.java
@@ -10,10 +10,5 @@ import java.util.List;
 import java.util.UUID;
 
 public interface FieldRepository extends JpaRepository<Field , UUID> {
-    List<Field> findByFarmUuid(UUID farmId);
-
-    @Query("SELECT SUM(f.area) FROM Field f WHERE f.farm.uuid = :farmId")
-    Double getTotalAreaByFarmUuid(UUID farmId);
-
     Page<Field> findAllByFarmUuid(UUID farmUuid, Pageable pageable);
 }
