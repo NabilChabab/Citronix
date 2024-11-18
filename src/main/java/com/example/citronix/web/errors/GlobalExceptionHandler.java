89852efd9c360d1523964a30995879ca4bf4ef2c
NabilChabab <nabil.chababnabil@gmail.com>
2024-11-18 17:20:34 +0100

    Quick Fix

--- src/main/java/com/example/citronix/web/errors/GlobalExceptionHandler.java
+++ src/main/java/com/example/citronix/web/errors/GlobalExceptionHandler.java
@@ -1,7 +1,6 @@
 package com.example.citronix.web.errors;
 
-import com.example.citronix.exceptions.ResourceNotFoundException;
-import com.example.citronix.exceptions.ValidationException;
+import com.example.citronix.exceptions.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -21,22 +20,79 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(errors , HttpStatus.BAD_REQUEST);
     }
 
+
+    @ExceptionHandler(FarmExistsException.class)
+    public ResponseEntity<Map<String, String>> handleFarmExistsException(FarmExistsException exception) {
+        Map<String, String> error = new HashMap<>();
+        error.put("error", exception.getMessage());
+        return new ResponseEntity<>(error, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(FarmNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handleFarmNotFoundException(FarmNotFoundException exception) {
+        Map<String, String> error = new HashMap<>();
+        error.put("error", exception.getMessage());
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(FarmSizeException.class)
+    public ResponseEntity<Map<String, String>> handleFarmSizeException(FarmSizeException exception) {
+        Map<String, String> error = new HashMap<>();
+        error.put("error", exception.getMessage());
+        return new ResponseEntity<>(error, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(FieldAreaException.class)
+    public ResponseEntity<Map<String, String>> handleFieldAreaException(FieldAreaException exception) {
+        Map<String, String> error = new HashMap<>();
+        error.put("error", exception.getMessage());
+        return new ResponseEntity<>(error, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(FieldNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handleFieldNotFoundException(FieldNotFoundException exception) {
+        Map<String, String> error = new HashMap<>();
+        error.put("error", exception.getMessage());
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(TreeNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handleTreeNotFoundException(TreeNotFoundException exception) {
+        Map<String, String> error = new HashMap<>();
+        error.put("error", exception.getMessage());
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(TreeHarvestedForSeason.class)
+    public ResponseEntity<Map<String, String>> handleTreeHarvestedForSeason(TreeHarvestedForSeason exception) {
+        Map<String, String> error = new HashMap<>();
+        error.put("error", exception.getMessage());
+        return new ResponseEntity<>(error, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(HarvestExistsForSeasonException.class)
+    public ResponseEntity<Map<String, String>> handleHarvestExistsForSeasonException(HarvestExistsForSeasonException exception) {
+        Map<String, String> error = new HashMap<>();
+        error.put("error", exception.getMessage());
+        return new ResponseEntity<>(error, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(HarvestPlantingException.class)
+    public ResponseEntity<Map<String, String>> handleHarvestPlantingException(HarvestPlantingException exception) {
+        Map<String, String> error = new HashMap<>();
+        error.put("error", exception.getMessage());
+        return new ResponseEntity<>(error, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(InvalidUuidException.class)
+    public ResponseEntity<Map<String, String>> handleInvalidUuidException(InvalidUuidException exception) {
+        Map<String, String> error = new HashMap<>();
+        error.put("error", exception.getMessage());
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<Map<String, String>> handleRuntimeException(RuntimeException exception) {
-        Map<String, String> error = new HashMap<>();
-        error.put("error", exception.getMessage());
-        return new ResponseEntity<>(error, HttpStatus.CONFLICT);
-    }
-
-    @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<Map<String, String>> handleResourceNotFoundException(ResourceNotFoundException exception) {
-        Map<String, String> error = new HashMap<>();
-        error.put("error", exception.getMessage());
-        return new ResponseEntity<>(error, HttpStatus.CONFLICT);
-    }
-
-    @ExceptionHandler(ValidationException.class)
-    public ResponseEntity<Map<String, String>> handleValidationException(ValidationException exception) {
         Map<String, String> error = new HashMap<>();
         error.put("error", exception.getMessage());
         return new ResponseEntity<>(error, HttpStatus.CONFLICT);
